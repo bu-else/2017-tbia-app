255bbbc2c3d880bbd848c82c77706ffbeebee107
lingyigu <echogu@bu.edu>
2017-10-15 13:27:35 -0400

    Create Home, History and Profile tabs (#3)

--- client/src/pages/history/history.ts
+++ client/src/pages/history/history.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-history',
+  templateUrl: 'history.html'
 })
-export class ContactPage {
+export class HistoryPage {
 
   constructor(public navCtrl: NavController) {
 
