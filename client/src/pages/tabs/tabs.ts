ad301018b7af5df500b47a206b1042aa9adbbb66
lingyigu <echogu@bu.edu>
2017-10-22 18:11:49 -0400

    Add LoginPage to Tabs for debugging purpose (#4)

--- client/src/pages/tabs/tabs.ts
+++ client/src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
+import { HomePage } from '../home/home';
 import { HistoryPage } from '../history/history';
 import { ProfilePage } from '../profile/profile';
-import { HomePage } from '../home/home';
+import { LoginPage} from '../login/login';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = HistoryPage;
   tab3Root = ProfilePage;
+  tab4Root = LoginPage;
 
   constructor() {
 
