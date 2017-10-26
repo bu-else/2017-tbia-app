7552a8a3936c08a5c8c0e5d1cac3aa00c5e0281a
Brian M. Roach <brian@Brians-iMac.local>
2017-10-25 22:50:51 -0400

    Added first time setup

--- server/setup_anchor.sh
+++ server/setup_anchor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Will install and run first time setup
 sudo apt-get update
 sudo apt-get install git
 sudo apt-get install nodejs
@@ -6,3 +7,4 @@ sudo apt-get install npm
 git clone git@github.com:hicsail/anchor.git
 cd anchor
 npm install
+npm run first-time-setup
