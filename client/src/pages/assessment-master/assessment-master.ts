22686ceee2244764ca870396b434dc57181a789a
lingyigu <echogu@bu.edu>
2017-11-08 17:28:23 -0500

    Modify the assessment tab (#18)

--- client/src/pages/assessment-master/assessment-master.ts
+++ client/src/pages/assessment-master/assessment-master.ts
@@ -11,6 +11,16 @@ export class AssessmentMasterPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  private assessments: any[] = [
+    { title: 'Face Emotion Recognition', component: 'AssessmentFaceEmotionRecognitionPage' },
+    { title: 'Changing Sets', component: '' },
+    { title: 'Drawing 3-D' , component: '' }
+  ];
+
+  startAssessment(page) {
+    this.navCtrl.push(page.component);
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad AssessmentMasterPage');
   }
