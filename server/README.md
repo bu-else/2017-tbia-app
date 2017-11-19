e13c7a3845461fbd241001dd140555bf2c49c47d
Brian M. Roach <brian@Brians-Mac-Pro.local>
2017-11-19 15:08:52 -0500

    Minor updates

---
+++ server/README.md
@@ -0,0 +1,5 @@
+
+
+## Development Notes:
+bcrypt is experimentally being swapped out for bcryptjs, using the instructions at:<br>
+https://github.com/jedireza/frame/wiki/bcrypt-Installation-Trouble
--- server/setup_anchor.sh
+++ server/setup_anchor.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt-get install nodejs
 sudo apt-get install npm
-git clone git@github.com:hicsail/anchor.git
+git clone https://github.com/hicsail/anchor.git
 cd anchor
 npm install
 npm run first-time-setup
