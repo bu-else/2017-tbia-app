666753090d4a3a8ccd4763398140422c237cc387
Brian M. Roach <brian@Brians-Mac-Pro.local>
2017-11-19 19:56:36 -0500

    Keeping a copy of anchor
    Decided to maintain our own copy of anchor because of modifications to
    bcrypt

--- back-end/setup_anchor.sh
+++ back-end/setup_anchor.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt-get install nodejs
 sudo apt-get install npm
-git clone https://github.com/hicsail/anchor.git
-cd anchor
+git clone -b Develop https://github.com/bu-else/2017-tbia-app.git
+cd ./anchor
 npm install
 npm run first-time-setup
--- server/README.md
+++
@@ -1,5 +0,0 @@
-
-
-## Development Notes:
-bcrypt is experimentally being swapped out for bcryptjs, using the instructions at:<br>
-https://github.com/jedireza/frame/wiki/bcrypt-Installation-Trouble
