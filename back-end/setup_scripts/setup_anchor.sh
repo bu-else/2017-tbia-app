5e01e5394a33f460b98c0c0bdcd2bd641265caec
Brian M. Roach <brian@Brians-Mac-Pro.local>
2017-11-19 21:13:47 -0500

    Move scripts into their own subdirectory

--- back-end/setup_scripts/setup_anchor.sh
+++ back-end/setup_scripts/setup_anchor.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Will install and run first time setup
 sudo apt-get update
-sudo apt-get install git
 sudo apt-get install nodejs
 sudo apt-get install npm
-git clone -b Develop https://github.com/bu-else/2017-tbia-app.git
-cd ./anchor
+cd ./../anchor
 npm install
 npm run first-time-setup
