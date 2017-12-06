a9739708f74f99a36d14b1624dca61f1be17c379
bmroach <bmroach23@gmail.com>
2017-12-06 13:01:47 -0500

    Old work in hapi

--- back-end/hapi/server.js
+++ back-end/hapi/server.js
@@ -1,34 +1,80 @@
 'use strict';
-
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/tbia";
 
 const Hapi = require('hapi');
 const server = new Hapi.Server({ port: 3000, host: 'localhost' });
 
+//___________________
+// (Get) Assessments
+//__________________
 server.route({
     method: 'GET',
     path: '/assessments',
     handler: function(request, h) {
-        results = {}
-        console.log("before");
 
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            db.collection("assessments").find({}).toArray(function(err, result) {
+
+        var promise = new Promise(function(resolve, reject) {
+            // do a thing, possibly async, then…
+            var output = 0;
+            MongoClient.connect(url, function(err, db) {
                 if (err) throw err;
-                console.log("in mongo call");
-                results = result;
-                db.close();
+                output = db.collection("assessments").find({});
             });
+
+            if (output != 0) {
+                resolve(output);
+            } else {
+                reject(Error("db promise error"));
+            }
         });
 
-        console.log("after");
-        return (results);
+        promise.then(function(result) { return (result) }, function(err) { console.log(err); });
     }
 });
 
 
+
+
+//___________________
+// (Get) Initial Survey
+//__________________
+
+
+
+
+
+
+
+
+
+
+
+//___________________
+// (Post) Assessment Responses
+//__________________
+
+
+
+
+
+
+
+
+
+//___________________
+// (Get) History
+// Note: On the back burner for now...
+//__________________
+
+
+
+
+
+
+
+
+//__________________
 server.start((err) => {
 
     if (err) {
