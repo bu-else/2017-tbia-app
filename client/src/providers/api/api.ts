214abba79a9e278104b56fa27acd938f189bf557
lingyigu <echogu@bu.edu>
2017-12-08 00:47:05 -0500

    Update api endpoint (#48)

--- client/src/providers/api/api.ts
+++ client/src/providers/api/api.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
  */
 @Injectable()
 export class Api {
-  url: string = 'https://example.com/api/v1';
+  url: string = 'http://128.31.25.58:9000';
 
   constructor(public http: HttpClient) {
   }
