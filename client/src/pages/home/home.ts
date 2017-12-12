9a535371e11b253afc73fc2ad164cf87c731efea
lingyigu <echogu@bu.edu>
2017-12-12 12:35:35 -0500

    Modify descriptions on the home page (#63)

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -11,6 +11,14 @@ export class HomePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  /**
+   * @function {toAssessmentTab}
+   * @return {void} {redirect to the assessment tab}
+   */
+  toAssessmentTab() {
+    this.navCtrl.parent.select(1);
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
