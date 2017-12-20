cb395cbbff68630762ad756d2b4b90919d0987cb
lingyigu <echogu@bu.edu>
2017-12-20 09:58:22 -0500

    Fix tests by adding the env provider (#72)

--- client/src/pages/signup/signup.spec.ts
+++ client/src/pages/signup/signup.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { IonicModule, NavController } from 'ionic-angular';
 import { MyApp } from '../../app/app.component';
 import { SignupPage } from './signup';
-import { Api, User } from '../../providers/providers';
+import { Api, User, EnvironmentProvider } from '../../providers/providers';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('Page: Sign Up', () => {
@@ -16,6 +16,7 @@ describe('Page: Sign Up', () => {
         NavController,
         Api,
         User,
+        EnvironmentProvider,
         HttpClient,
         HttpHandler
       ],
